fc937d6e9b638564135bc1e01aba9eef1834c264
Charles <Charles@Apollo>
2012-05-12 23:08:57 -0700

    Removed two projects

--- Source/Momntz/CommandHandlers/Users/CreateUserCommandHandler.cs
+++ Source/Momntz/CommandHandlers/Users/CreateUserCommandHandler.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Hypersonic;
+using Hypersonic.Session;
+using Momntz.Commands.Users;
 
 namespace Momntz.CommandHandlers.Users
 {
-    class CreateUserCommandHandler
+    public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand>
     {
+        private readonly IDatabase _database;
+
+        /// <summary> Constructor. </summary>
+        /// <param name="database"> The database. </param>
+        public CreateUserCommandHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        /// <summary> Handles. </summary>
+        /// <param name="command"> The command. </param>
+        public void Handles(CreateUserCommand command)
+        {
+            using (ISession session = SessionFactory.SqlServer())
+            {
+                session.Save(command, "User");
+            }
+        }
     }
 }
