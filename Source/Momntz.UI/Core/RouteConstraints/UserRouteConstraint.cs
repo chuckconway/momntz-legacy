c4560d55b60bcc10102689036927b444aa1b07bc
Chuck <Chuck@localhost>
2012-05-23 21:40:23 -0700

    Removed unneeded files

--- Source/Momntz.UI/Core/RouteConstraints/UserRouteConstraint.cs
+++ Source/Momntz.UI/Core/RouteConstraints/UserRouteConstraint.cs
@@ -12,20 +12,6 @@ namespace Momntz.UI.Core.RouteConstraints
     {
         private readonly IProjectionProcessor _processor;
 
-        /// <summary> 
-        /// Determines whether the URL parameter contains a valid value for this constraint.
-        /// </summary>
-        /// <param name="processor"> The processor. </param>
-        ///
-        /// ### <returns> true if the URL parameter contains a valid value; otherwise, false. </returns>
-        /// ### <param name="httpContext">
-        /// An object that encapsulates information about the HTTP request. </param>
-        /// ### <param name="route">          The object that this constraint belongs to. </param>
-        /// ### <param name="parameterName">  The name of the parameter that is being checked. </param>
-        /// ### <param name="values">         An object that contains the parameters for the URL. </param>
-        /// ### <param name="routeDirection">
-        /// An object that indicates whether the constraint check is being performed when an incoming
-        /// request is being handled or when a URL is being generated. </param>
         public UserRouteConstraint(IProjectionProcessor processor)
         {
             _processor = processor;
---
+++ Source/Momntz/Model/Tag.cs
@@ -0,0 +1,46 @@
+﻿namespace Momntz.Model
+{
+    public class Tag
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>
+        /// The id.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the story.
+        /// </summary>
+        /// <value>
+        /// The story.
+        /// </value>
+        public string Story { get; set; }
+
+        /// <summary>
+        /// Gets or sets the kind of tag.
+        /// </summary>
+        /// <value>
+        /// The kind of tag.
+        /// </value>
+        public KindOfTag KindOfTag { get; set; }
+
+    }
+
+    public enum KindOfTag
+    {
+        User,
+        Tag,
+        Location,
+        Album
+    }
+}
