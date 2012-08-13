803fe43be1f9b5b6828234626b4840e33f78e456
Chuck Conway <chuckconway@users.noreply.github.com>
2012-08-12 23:16:43 -0700

    Pulled in the Database changes

---
+++ Source/Momntz.Database/dbo/Stored Procedures/TagAlbum_Add.sql
@@ -0,0 +1,50 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[TagAlbum_Add]
+	-- Add the parameters for the stored procedure here
+(
+	@Tag nvarchar(50),
+	@MomentoId int,
+	@Username nvarchar(100)
+)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+
+
+	Declare @AlbumId int
+	Set @AlbumId = null
+
+	If Exists (Select T.Id From Tag T Where T.Name = @Tag AND T.Kind = 3 AND T.Username = @Username)
+	BEGIN
+		Set @AlbumId = (Select T.Id From Tag T Where T.Name = @Tag AND T.Kind = 3 AND T.Username = @Username)
+
+		Declare @TagCount int
+
+		SET @TagCount = (Select Count(*)
+		FROM TagAlbum T
+		Where T.MomentoId = @MomentoId
+			AND T.TagId = @AlbumId)
+
+		IF @TagCount = 0
+			BEGIN
+				Insert Into TagAlbum(MomentoId, TagId)Values(@MomentoId, @AlbumId)
+				Insert Into TagMomento(MomentoId, TagId)Values(@MomentoId, @AlbumId)
+			END
+	END
+	Else
+		BEGIN
+			Insert Into Tag (Name, Kind, Username) Values(@Tag, 3, @Username)
+			Set @AlbumId = SCOPE_IDENTITY()
+
+			Insert Into TagAlbum(MomentoId, TagId)Values(@MomentoId, @AlbumId)
+			Insert Into TagMomento(MomentoId, TagId)Values(@MomentoId, @AlbumId)
+		END
+END
---
+++ Source/Momntz.Database/dbo/Stored Procedures/TagAlbum_GetTermByFirstCharactersAndUsername.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE TagAlbum_GetTermByFirstCharactersAndUsername
+	-- Add the parameters for the stored procedure here
+(
+	@Term nvarchar(50),
+	@Username nvarchar(100)	
+)
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	Select Name
+	From TagAlbumView T
+	Where T.Name LiKe @Term + '%'
+		AND T.Username = @Username
+END
