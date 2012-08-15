1d2f0559e154a40644d692f75ce785acf03cf18d
Chuck <Chuck@localhost>
2012-08-15 08:48:40 -0700

    Added the signup pages.

---
+++ Source/Momntz.UI/Controllers/SignupController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Momntz.UI.Models.Login;
+
+namespace Momntz.UI.Controllers
+{
+    public class SignupController : Controller
+    {
+        //
+        // GET: /Signup/
+
+        public ActionResult Index()
+        {
+            return View(new LoginView());
+        }
+
+    }
+}
