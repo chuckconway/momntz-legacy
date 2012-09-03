75a3e4ce46ee762eb1d74c35c02367895d573bdb
Chuck Conway <chuckconway@users.noreply.github.com>
2012-09-03 15:45:00 -0700

    Moved to a new solution

--- Source/Momntz.Worker.Core/Implementations/Media/MediaTypes/IMediaTypes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Momntz.Worker.Core.Implementations.Media.MediaTypes
-{
-    public interface IMedia
-    {
-        string Media { get; }
-
-        void Process(MediaItem message);
-    }
-}
