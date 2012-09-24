0fc1c326879c4043247b8f456887458ff63b74f3
Chuck <Chuck@localhost>
2012-09-23 18:22:34 -0700

    ghfb

--- Source/Momntz.Queue/dbo/Tables/Queue.sql
+++ Source/Momntz.Queue/dbo/Tables/Queue.sql
@@ -4,7 +4,7 @@
     [Payload]        NVARCHAR (MAX)  NOT NULL,
     [CreateDate]     DATETIME        CONSTRAINT [DF_Queue_CreateDate] DEFAULT (getutcdate()) NOT NULL,
     [MessageStatus]  NVARCHAR (50)   NOT NULL,
-    [Error]          NVARCHAR (2000) NULL,
+    [Error]          NVARCHAR (MAX)  NULL,
     [CompleteDate]   DATETIME        NULL,
     CONSTRAINT [PK_Queue] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
