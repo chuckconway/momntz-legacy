4ac1df76ceab4efcd00ca2a0d9887fe1e63c7bbc
Chuck Conway <chuckconway@users.noreply.github.com>
2013-03-19 21:49:30 -0700

    fixed the the settings issues

---
+++ Source/Momntz/Data/Mappings/ExifMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+using Momntz.Model;
+
+namespace Momntz.Data.Mappings
+{
+    public class ExifMap : ClassMap<Exif>
+    {
+        public ExifMap()
+        {
+            Id(e => e.Id);
+
+            Map(e => e.Key,"[Key]");
+
+            Map(e => e.Type);
+
+            Map(e => e.Value);
+
+            References(m => m.Momento, "MomentoId");
+        }
+    }
+}
