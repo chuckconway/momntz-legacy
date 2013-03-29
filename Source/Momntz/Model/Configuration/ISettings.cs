823251bc84c77d9d6b6afd001a3988e11788faa2
Chuck <Chuck@localhost>
2013-03-28 21:52:20 -0700

    Added AjaxMin as a reference

--- Source/Momntz/Model/Configuration/ISettings.cs
+++ Source/Momntz/Model/Configuration/ISettings.cs
@@ -2,8 +2,28 @@
 {
     public interface ISettings
     {
+        /// <summary>
+        /// Gets the cloud URL.
+        /// </summary>
+        /// <value>The cloud URL.</value>
         string CloudUrl { get; }
 
+        /// <summary>
+        /// Gets the cloud account.
+        /// </summary>
+        /// <value>The cloud account.</value>
+        string CloudAccount { get; }
+
+        /// <summary>
+        /// Gets the cloud key.
+        /// </summary>
+        /// <value>The cloud key.</value>
+        string CloudKey { get; }
+
+        /// <summary>
+        /// Gets the queue database.
+        /// </summary>
+        /// <value>The queue database.</value>
         string QueueDatabase { get; }
 
         int ImageSmallWidth { get; }
