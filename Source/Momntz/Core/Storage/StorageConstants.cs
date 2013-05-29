0ec9d16c66032019ab8335cda44908f79a25c15f
Chuck <Chuck@localhost>
2013-05-28 18:05:15 -0700

    New Cloud files

---
+++ Source/Momntz/Core/Storage/StorageConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace Momntz.Core.Storage
+{
+    public class StorageConstants
+    {
+        /// <summary>
+        /// The queue bucket
+        /// </summary>
+        public const string QueueBucket = "queue";
+
+        /// <summary>
+        /// The image bucket
+        /// </summary>
+        public const string ImageBucket = "img";
+    }
+}
