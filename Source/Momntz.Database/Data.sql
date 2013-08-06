ce40667531fe26e1d37a138a9e2fa4f0475b0966
Chuck Conway <chuckconway@users.noreply.github.com>
2013-08-05 21:27:05 -0700

    Added new release SQL script

--- Source/Momntz.Database/Data.sql
+++ Source/Momntz.Database/Data.sql
@@ -1,7 +1,4 @@
 ﻿GO
-SET IDENTITY_INSERT [dbo].[Configuration] ON 
-
-GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (2, N'Image.Small.Width', N'240', NULL)
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (3, N'Image.Small.Height', N'180', NULL)
@@ -14,15 +11,11 @@ INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (6, N
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (7, N'Image.Large.Height', N'1080', NULL)
 GO
-INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (9, N'Database.Queue', N'Data Source=localhost;Initial Catalog=LocalQueue;User Id=momntz;Password=folsom_1;', N'LOCAL')
-GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (10, N'cloudurl', N'http://127.0.0.1:10000/devstoreaccount1', N'LOCAL')
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (11, N'cloudaccount', N'devstoreaccount1', N'LOCAL')
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (12, N'cloudkey', N'Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==', N'LOCAL')
-GO
-INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1003, N'Database.Queue', N'Server=tcp:gha9fb98fk.database.windows.net,1433;Database=Momntz.Queue;User ID=cconway;Password=Austin_1;Trusted_Connection=False;Encrypt=True;Connection Timeout=30', N'PROD')
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1004, N'cloudurl', N'http://momntz.blob.core.windows.net', N'PROD')
 GO
@@ -30,21 +23,21 @@ INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1005
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1006, N'cloudkey', N'k9migT9gDlx2lzwLw+nXvq7AzFYdoPwhjUyQeHA/hDz37skLB4flSS79sD3MC7EGqwWBjfCKwtgPVyrOn692Jw==', N'PROD')
 GO
-INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1007, N'Database.Queue', N'Data Source=Apollo;Initial Catalog=QaMomntz.Queue;User Id=momntz;Password=folsom_1;', N'DEV')
-GO
-INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1008, N'cloudurl', N'http://devmomntz.blob.core.windows.net', N'DEV')
-GO
-INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1009, N'cloudaccount', N'devmomntz', N'DEV')
-GO
-INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1010, N'cloudkey', N'3457ReUKnJTXMj57nh3/m1RozhSF+hHFCX1QYhsSOiUwh3dVEbgSrDwhC1x+0QbKxzRzbH7kLb9i+HxVCPYSnw==', N'DEV')
-GO
-INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1011, N'Database.Queue', N'Data Source=Apollo;Initial Catalog=QaMomntz.Queue;User Id=momntz;Password=folsom_1;', N'QA')
-GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1012, N'cloudurl', N'http://qamemorablemoments.blob.core.windows.net', N'QA')
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1013, N'cloudaccount', N'qamemorablemoments', N'QA')
 GO
 INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1014, N'cloudkey', N'9E1t/udGpskjgHd2Vhy76RoovFenWVORv3noM/Svr2bdC1UkB/iGcdBup46jbwGdk+fLcoa1URtLnkivMDXyEw==', N'QA')
 GO
-SET IDENTITY_INSERT [dbo].[Configuration] OFF
+INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1015, N'ServiceBus.Queue', N'Endpoint=sb://momntzdev.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=TU9SBGWvdxeO6THM8uQrxA9fmJEhTlLHha80rTkNj7Y=', N'LOCAL')
+GO
+INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1016, N'ServiceBus.Queue', N'Endpoint=sb://momntzqa.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=IbgyP8beHL15QtPGUN3KKFsogMUozdeX7603EF7xd40=', N'QA')
+GO
+INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1017, N'ServiceBus.Queue', N'Endpoint=sb://momntz.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=6d1C5tpI4JRCUqWZ1QxIx+4+M1uYZZgZiR2lYL2ieXw=', N'PROD')
+GO
+INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1018, N'LogToFile', N'c:\logs\service.log', N'LOCAL')
+GO
+INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1019, N'LogToFile', N'c:\logs\service.log', N'QA')
+GO
+INSERT [dbo].[Configuration] ([Id], [Name], [Value], [Environment]) VALUES (1020, N'LogToFile', N'c:\logs\service.log', N'PROD')
 GO
