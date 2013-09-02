0c6ad1ae55357d2bb12e76de2793d91c3d1bbd3f
Chuck Conway <chuckconway@users.noreply.github.com>
2013-09-02 15:07:27 -0700

    move all masterview javascript out of page into js file.

---
+++ Source/Momntz.UI/Scripts/views/masterview/master.js
@@ -0,0 +1,103 @@
+﻿
+var container = $('#container');
+
+function addTiles(data) {
+
+    if (data != null && data.length > 0) {
+        var source = getTemplate();
+        var template = Handlebars.compile(source);
+
+        var html = '';
+
+        for (var index = 0; index < data.length; index++) {
+
+            html = html + template(data[index]);
+        }
+
+        html = $($.trim(html));
+
+        html.imagesLoaded(function () {
+            $('div#loading').remove();
+            container.append(html);
+            container.masonry('appended', html, true);
+        });
+    } else {
+        $('div#loading').remove();
+    }
+}
+
+function prependTiles(data) {
+
+    if (data != null && data.length > 0) {
+
+        var source = getTemplate();
+        var template = Handlebars.compile(source);
+
+        var html = '';
+
+        for (var index = 0; index < data.length; index++) {
+
+            html = html + template(data[index]);
+        }
+
+        html = $($.trim(html));
+
+        html.imagesLoaded(function () {
+            $('div#loading').remove();
+            container.prepend(html);
+            container.masonry('prepended', html, true);
+        });
+    } else {
+        $('div#loading').remove();
+    }
+}
+
+
+$(window).scroll(function () {
+
+    var scrollTop = $(window).scrollTop();
+    var documentHeight = $(document).height();
+    var windowHieght = $(window).height();
+
+    if (scrollTop == (documentHeight - windowHieght)) {
+
+        if ($('div#loading').length == 0) {
+
+            $('<div id="loading"><img src="/Content/images/spinner.gif" title="Spinner " /></div>').insertAfter(container);
+
+            getItems(addTiles);
+        }
+    }
+});
+
+$(function () {
+
+    $('button#addimages').click(function () {
+        if ($("div#add").is(":hidden")) {
+            $("div#add").show("slow");
+        } else {
+            $("div#add").slideUp();
+        }
+    });
+
+    container.imagesLoaded(function () {
+        container.masonry({
+            itemSelector: '.box',
+            columnWidth: 10
+        });
+    });
+
+    // Lightbox Init
+    var fancyboxArgs = {
+        padding: 0,
+        overlayColor: "#000",
+        autoScale: false,
+        overlayOpacity: 0.85,
+        loadDetails: function (c, image) {
+
+            var view = new afterLoad();
+            view.load(this, c, image);
+        }
+    };
+    $("a[rel='lightbox']").fancybox(fancyboxArgs);
+});
